7dc8b8120b7bad699b243c61ace847209e1aa735
Krishna <iamkrishna.krishna@gmail.com>
2022-01-22 21:28:26 +0530

    Added local check - bug fix

--- src/pages/FinishAuth.js
+++ src/pages/FinishAuth.js
@@ -1,4 +1,5 @@
 import {useSearchParams} from "react-router-dom";
+import {checkIsLocal} from "../utils";
 
 function setCookie(name,value,hours) {
     var expires = "";
@@ -17,7 +18,8 @@ const FinishAuth = (props) => {
     const code  = searchParams.get("access_token")
     console.log(`access_token: ${code}`)
     setCookie('access_token',code,1)
-    window.location.replace('http://localhost:3000/');
+    const redirectURL = checkIsLocal() ? "http://localhost:3000/auth" : "http://3.65.240.94:3080/auth"
+    window.location.replace(redirectURL);
     return null;
 }
 
--- src/pages/HomePageBase.js
+++ src/pages/HomePageBase.js
@@ -4,7 +4,7 @@ import {checkIsLocal} from "../utils";
 
 const getSpotifyUserLogin = () => {
     const authURL = checkIsLocal() ? "http://localhost:8080/auth" : "http://3.65.240.94:8080/auth"
-    fetch(authURL)
+    fetch(authURL,{mode: "no-cors"})
         .then((response) => response.text())
         .then(response => {
             console.log('Redirect: '+response.toString())
